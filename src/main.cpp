febe659425ff73e5af900a4d22cdbfc1e189d4e4
froleyks <nfroleyks@gmail.com>
2019-05-06 15:29:06 +0200

    name change

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
   ParameterProcessor params(argc, argv);
   if (params.getFilename() == "") {
-    std::cout << "USAGE: ./cegear_<solver> <planning.sas>\n" << std::endl;
+    std::cout << "USAGE: ./ADAL_<solver> <planning.sas>\n" << std::endl;
     params.printDefaults();
     return 1;
   }
